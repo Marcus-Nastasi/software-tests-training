01f8180d607f43f432f6293b19ef5441ff790ae0
rolemberg <vinnie.nsts@gmail.com>
2024-05-22 14:59:45 -0300

    creating SavingsAccount class methods;

--- src/main/java/com/santander/tests/Domain/Account/SavingsAccount.java
+++ src/main/java/com/santander/tests/Domain/Account/SavingsAccount.java
@@ -1,26 +1,35 @@
 package com.santander.tests.Domain.Account;
 
 import com.santander.tests.Domain.Client.Person;
+import com.santander.tests.Exceptions.AccountException;
 
 public class SavingsAccount extends Account {
+
+    private final double profitPercentual = 0.1;
 
     public SavingsAccount(long id, long agency, Person client) {
         super(id, agency, client);
     }
 
-    @Override
-    public void withdraw(double value) {
-
+    public void profit() {
+        this.setBalance(this.getBalance() + (this.getBalance() * this.profitPercentual));
     }
+
+    @Override
+    public void withdraw(double value) throws AccountException {
+        if (this.getBalance() < value) throw new AccountException("Balance lower than value");
+        this.setBalance(this.getBalance() - value);
+}
 
     @Override
     public void deposit(double value) {
-
+        this.setBalance(this.getBalance() + value);
     }
 
     @Override
-    public void transferBetweenAccounts(Account accountTo, double value) {
-
+    public void transferBetweenAccounts(Account accountTo, double value) throws AccountException {
+        this.withdraw(value);
+        accountTo.deposit(value);
     }
 }
 
