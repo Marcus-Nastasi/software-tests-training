36edbedfb944c95a250fa650c6d4ec2fd5781e21
rolemberg <vinnie.nsts@gmail.com>
2024-05-22 13:57:07 -0300

    created class Person;

--- src/main/java/com/santander/tests/Domain/Client/Person.java
+++ src/main/java/com/santander/tests/Domain/Client/Person.java
@@ -2,5 +2,26 @@ package com.santander.tests.Domain.Client;
 
 public abstract class Person {
 
+    private final String name;
+    private final String cpf;
 
+    public Person(String name, String cpf) {
+        this.name = name;
+        this.cpf = cpf;
+    }
+
+    @Override
+    public String toString() {
+        return("Person{" + "name='" + name + '\'' + ", cpf='" + cpf + '\'' + '}');
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
 }
+
+
