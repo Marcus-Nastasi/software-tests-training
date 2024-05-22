a98cdc05c6f91c58f6046bee4974580ea9a5de87
rolemberg <vinnie.nsts@gmail.com>
2024-05-22 14:24:34 -0300

    testing CurrentAccount constructor;

--- src/test/java/CurrentAccountTests.java
+++ src/test/java/CurrentAccountTests.java
@@ -1,10 +1,19 @@
+import com.santander.tests.Domain.Account.Account;
+import com.santander.tests.Domain.Account.CurrentAccount;
+import com.santander.tests.Domain.Client.Client;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class CurrentAccountTests {
 
+    private final Client client = new Client("Henry", "122222222");
+    private final Account account = new CurrentAccount(1000, 1004, client);
+
     @Test
     void testingConstructor() {
-
+        Assertions.assertEquals(1000, account.getId());
+        Assertions.assertEquals(1004, account.getAgency());
+        Assertions.assertEquals(client, account.getClient());
     }
 }
 
