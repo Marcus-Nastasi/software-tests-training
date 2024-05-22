09213efe5bde28ff1a2ff03abdbcb14018a3bccf
rolemberg <vinnie.nsts@gmail.com>
2024-05-22 15:17:04 -0300

    little refactoring on ClientTests;

--- src/test/java/ClientTests.java
+++ src/test/java/ClientTests.java
@@ -1,5 +1,6 @@
 import com.santander.tests.Domain.Account.Account;
 import com.santander.tests.Domain.Account.CurrentAccount;
+import com.santander.tests.Domain.Account.SavingsAccount;
 import com.santander.tests.Domain.Client.Client;
 import com.santander.tests.Exceptions.ClientException;
 import org.junit.jupiter.api.Assertions;
@@ -9,6 +10,7 @@ public class ClientTests {
 
     private final Client person = new Client("Joshua", "33457765");
     private final Account account = new CurrentAccount(1000, 1004, person);
+    private final Account account2 = new SavingsAccount(1002, 1004, person);
 
     @Test
     void testingConstructor() {
@@ -19,9 +21,12 @@ public class ClientTests {
     @Test
     void testingAddingAccount() {
         person.addAccount(account);
+        person.addAccount(account2);
         Assertions.assertEquals(1000, person.getAccountList().getFirst().getId());
         Assertions.assertEquals(1004, person.getAccountList().getFirst().getAgency());
         Assertions.assertEquals(person, person.getAccountList().getFirst().getClient());
+        Assertions.assertTrue(person.getAccountList().getFirst() instanceof CurrentAccount);
+        Assertions.assertTrue(person.getAccountList().getLast() instanceof SavingsAccount);
     }
 
     @Test
